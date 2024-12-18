e90dacf4e8ba67b212ebf7af7fc0550614a170b0
akmaladib01 <akmaladib15801@gmail.com>
2024-12-18 15:01:26 +0800

    update

--- main.js
+++ main.js
@@ -23,7 +23,13 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadURL(`file://${__dirname}/dist/zakat/browser/index.html`);
+  const startURL = app.isPackaged
+    ? `file://${path.join(__dirname, 'browser', 'index.html')}` // For packaged app
+    : `file://${path.join(__dirname, 'dist/zakat/browser/index.html')}`; // Development mode
+
+  console.log('Loading URL:', startURL);
+  mainWindow.loadURL(startURL);
+
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
